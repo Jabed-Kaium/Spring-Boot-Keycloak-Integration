552c2e88b926f40fd22d53001cd46073a24a8748
Jabed-Kaium <jabedkaium@gmail.com>
2025-01-01 17:58:11 +0600

    first commit

---
+++ src/test/java/org/example/keycloakspringboot/KeycloakSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.keycloakspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
